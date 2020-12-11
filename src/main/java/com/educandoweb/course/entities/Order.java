0d8ba734b72bf18601bc8aa634e181e0dce6d99e
Luiz Fernando Angeli <fernando_angeli@hotmail.com>
2020-12-11 00:04:21 -0300

    Category

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
